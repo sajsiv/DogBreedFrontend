7d3914f402c41d807b9809cb51b158bc73a383fb
sajsiv <sajjansivia@gmail.com>
2022-05-04 12:11:22 +0100

    fixing test

--- src/utils/urlBreedExtractor.ts
+++ src/utils/urlBreedExtractor.ts
@@ -2,7 +2,3 @@ export default function urlBreedExtractor(endpoint: string) {
   const endpointArr = endpoint.split("/");
   return endpointArr[4];
 }
-
-urlBreedExtractor(
-  "https://images.dog.ceo/breeds/hound-english/n02089973_3055.jpg"
-);
